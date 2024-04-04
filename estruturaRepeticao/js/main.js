abc53d10385e6398428d26b991316237a0bb53fd
Arley Siaci Augusto <arley.saugusto@sp.senac.br>
2024-04-04 17:32:11 -0300

    Entrega do quadro Estrutura de Repetição - Jogo do Pim

--- estruturaRepeticao/js/main.js
+++ estruturaRepeticao/js/main.js
@@ -1,16 +1,19 @@
 const solucaoParImpar = document.querySelector('#ex1');
 const solucaoMultiplo3 = document.querySelector('#ex2');
 const solucaoContagemRegressiva = document.querySelector('#ex3');
+const solucaoJogoPim = document.querySelector('#ex4');
 
 let resultadoParImpar = mostrarParImpar(0, 50);
 solucaoParImpar.innerHTML = resultadoParImpar;
 
-let resultadoMultiplo3 = mostrarMultiplos(0,100,3);
+let resultadoMultiplo3 = mostrarMultiplos(0, 100, 3);
 solucaoMultiplo3.innerHTML = resultadoMultiplo3;
 
-let contagemRegressiva = mostrarContagemRegressiva(100,0,2);
-solucaoContagemRegressiva.innerHTML=contagemRegressiva;
+let contagemRegressiva = mostrarContagemRegressiva(100, 0, 2);
+solucaoContagemRegressiva.innerHTML = contagemRegressiva;
 
+let resultadoJogoPim = mostrarJogoPim(40);
+solucaoJogoPim.innerHTML = resultadoJogoPim;
 
 function mostrarParImpar(numInicial, numFinal) {
     let resultado = `${numInicial} - ${retornarTextoParImpar(numInicial)}<br>`;
@@ -33,7 +36,7 @@ function retornarTextoParImpar(numero) {
 }
 
 function mostrarMultiplos(numInicial, numFinal, numReferencia) {
-    let resultado="";
+    let resultado = "";
     for (let i = numInicial; i <= numFinal; i++) {
         if (i % numReferencia == 0) {
             resultado = `${resultado}${i}<br>`;
@@ -49,10 +52,30 @@ function mostrarMultiplos(numInicial, numFinal, numReferencia) {
     return resultado;
 }
 
-function mostrarContagemRegressiva(numInicial, numFinal,passo){
-    let resultado="";
-    for (let i = numInicial; i>=numFinal; i=i-passo) {
-       resultado=`${resultado}<br>${i}`;   
+function mostrarContagemRegressiva(numInicial, numFinal, passo) {
+    let resultado = "";
+    for (let i = numInicial; i >= numFinal; i = i - passo) {
+        resultado = `${resultado}<br>${i}`;
+    }
+    return resultado;
+
+    // let j=numInicial;
+    // while (j>=numFinal) {
+    //     resultado = `${resultado}<br>${i}`;
+    //     j=j-passo;
+    // }
+}
+
+function mostrarJogoPim(numFinal) {
+    let resultado = "";
+    let txtAcrescentar = "<br>"
+    for (let i = 1; i <= numFinal; i++) {
+        i == numFinal ? txtAcrescentar = "!" : "";
+        if (i % 4 == 0) {
+            resultado = `${resultado}PIM${txtAcrescentar}`;
+        } else {
+            resultado = `${resultado}${i}${txtAcrescentar}`;
+        }
     }
     return resultado;
 }
