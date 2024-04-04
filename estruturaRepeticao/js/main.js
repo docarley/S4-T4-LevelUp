9f290403ffc17a2eefef1c69dd62f71aef2bb6bd
Arley Siaci Augusto <arley.saugusto@sp.senac.br>
2024-04-04 16:58:10 -0300

    Entrega do quadro Estrutura de Repetição - Múltiplos de 3

--- estruturaRepeticao/js/main.js
+++ estruturaRepeticao/js/main.js
@@ -1,12 +1,16 @@
-const solucaoParImpar = document.querySelector('.exercicio__solucao-txt');
+const solucaoParImpar = document.querySelector('#ex1');
+const solucaoMultiplo3 = document.querySelector('#ex2');
 
 let resultadoParImpar = mostrarParImpar(0, 50);
 solucaoParImpar.innerHTML = resultadoParImpar;
 
+let resultadoMultiplo3 = mostrarMultiplos(0,100,3);
+solucaoMultiplo3.innerHTML = resultadoMultiplo3;
+
 
 function mostrarParImpar(numInicial, numFinal) {
     let resultado = `${numInicial} - ${retornarTextoParImpar(numInicial)}<br>`;
-    for (let i = numInicial+1; i <= numFinal; i++) {
+    for (let i = numInicial + 1; i <= numFinal; i++) {
         resultado = `${resultado}${i} - ${retornarTextoParImpar(i)}<br>`;
     }
     // let j=numInicial;
@@ -17,9 +21,26 @@ function mostrarParImpar(numInicial, numFinal) {
     return resultado;
 }
 
-function retornarTextoParImpar(numero){
-    if (numero%2==0) {
+function retornarTextoParImpar(numero) {
+    if (numero % 2 == 0) {
         return "Par";
     }
     return "Impar";
+}
+
+function mostrarMultiplos(numInicial, numFinal, numReferencia) {
+    let resultado="";
+    for (let i = numInicial; i <= numFinal; i++) {
+        if (i % numReferencia == 0) {
+            resultado = `${resultado}${i}<br>`;
+        }
+    }
+    // let j=numInicial;
+    // while (j<=numFinal) {
+    // if (i % numReferencia == 0) {
+    //     resultado = `${resultado}${i}<br>`;
+    // }
+    //     j++;
+    // }
+    return resultado;
 }
